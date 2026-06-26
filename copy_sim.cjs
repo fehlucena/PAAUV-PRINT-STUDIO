3be1e12e3d55bf3738bbc45adf790e8908518bb8
fehlucena <53108060+fehlucena@users.noreply.github.com>
2026-06-26 11:09:08 -0300

    limpando arquivos errados

--- copy_sim.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let code = fs.readFileSync('src/components/SimuladorIA.tsx', 'utf8');
-
-// Replace export default function SimuladorIA with export default function ProdutoForm
-code = code.replace('export default function SimuladorIA()', 'export default function ProdutoForm()');
-
-// Write to a new file so I can review or directly to ProdutoForm
-fs.writeFileSync('src/components/ProdutoForm.tsx.tmp', code);
